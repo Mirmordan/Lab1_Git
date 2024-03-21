b4e428d4c8eed167fd1eb1e0ca0662cd18b35073
mirmordan <mrdanmorro99@gmail.com>
2024-03-21 09:13:05 +0300

    Add Circle

--- prog.cpp
+++ prog.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES 
 #include <cmath>
 #include <iostream>
 using namespace std;
@@ -9,22 +10,31 @@ int switch_key;
 cin >> switch_key;
 
 switch(switch_key){
-    case 1:{
-        wcout << L"Введите длины трёх сторон треугольника :" << endl;
-        double edge1,edge2,edge3;
-        cin >> edge1 >> edge2 >> edge3;
-        wcout << L"Периметр : " << edge1+edge2+edge3 <<endl;
-        double half_per = (edge1+edge2+edge3)/2;
-        double square = sqrt(half_per*(half_per-edge1)*(half_per-edge2)*(half_per-edge3));
-        wcout << L"Площадь : " << square <<endl;
-        if ( edge1==edge2 or edge2==edge3 or edge1==edge3)
-        wcout << L"Треугольник равнобедренный" <<endl;
-        else
-        wcout << L"Треугольник не равнобедренный" <<endl;
-        break; 
-        }
-    case 2:{
-
+case 1:{
+wcout << L"Введите длины трёх сторон треугольника :" << endl;
+double edge1,edge2,edge3;
+cin >> edge1 >> edge2 >> edge3;
+wcout << L"Периметр : " << edge1+edge2+edge3 <<endl;
+double half_per = (edge1+edge2+edge3)/2;
+double square = sqrt(half_per*(half_per-edge1)*(half_per-edge2)*(half_per-edge3));
+wcout << L"Площадь : " << square <<endl;
+if ( edge1==edge2 or edge2==edge3 or edge1==edge3)
+wcout << L"Треугольник равнобедренный" <<endl;
+else
+wcout << L"Треугольник не равнобедренный" <<endl;
+break; 
+}
+case 2: {
+wcout << L"Введите параметры окружности (радиус, угол кругового сектора) :" << endl;
+double radius,degree;
+cin >> radius >> degree;
+double Square = M_PI * radius * radius;
+wcout << L"Длинна окружности : " << 2 * M_PI * radius << endl;
+wcout << L"Площадь окружности : " << Square << endl;
+if (degree <0 or degree >360) 
+wcout << L"Площадь кругового сектора не соответствует объективной действительности" << endl;
+else
+wcout << L"Площадь кругового сектора : " << Square*degree/360 << endl;
 break;
 }
 default:
