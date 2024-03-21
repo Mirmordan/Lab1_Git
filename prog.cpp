e6f0cc97a15d69772c35496bb4144a7eafdf0810
root <root@.localdomain>
2024-03-21 12:54:26 +0700

    added treugolnik

--- prog.cpp
+++ prog.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -8,11 +9,21 @@ int switch_key;
 cin >> switch_key;
 
 switch(switch_key){
-case 1:{
-
-break; 
-}
-case 2:{
+    case 1:{
+        wcout << L"Введите длины трёх сторон треугольника :" << endl;
+        double edge1,edge2,edge3;
+        cin >> edge1 >> edge2 >> edge3;
+        wcout << L"Периметр : " << edge1+edge2+edge3 <<endl;
+        double half_per = (edge1+edge2+edge3)/2;
+        double square = sqrt(half_per*(half_per-edge1)*(half_per-edge2)*(half_per-edge3));
+        wcout << L"Площадь : " << square <<endl;
+        if ( edge1==edge2 or edge2==edge3 or edge1==edge3)
+        wcout << L"Треугольник равнобедренный" <<endl;
+        else
+        wcout << L"Треугольник не равнобедренный" <<endl;
+        break; 
+        }
+    case 2:{
 
 break;
 }
